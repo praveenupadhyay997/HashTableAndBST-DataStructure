8ea977c020c7544c6c6e5ceeaeb4e069d8aba22d
praveenupadhyay997 <praveenupadhyay997@gmail.com>
2020-10-25 23:19:45 +0530

    [Praveen] Add: Implemented the functionality to delete the avoidable words from the sentence using the hash set.

--- HashTableAndBSTProblem/HashTableAndBSTProblem/HashTableMap.cs
+++ HashTableAndBSTProblem/HashTableAndBSTProblem/HashTableMap.cs
@@ -111,5 +111,28 @@ namespace HashTableAndBSTProblem
                     }
             }
         }
+        /// <summary>
+        /// UC3- To remove the avoidable words in a sentence by passing the key of the removing element
+        /// </summary>
+        /// <param name="key"></param>
+        public void Remove(K key)
+        {
+            int position = GetArrayPosition(key);
+            LinkedList<KeyValue<K, V>> linkedList = GetLinkedList(position);
+            bool itemFound = false;
+            KeyValue<K, V> foundItem = default(KeyValue<K, V>);
+            foreach (KeyValue<K, V> item in linkedList)
+            {
+                if (item.Key.Equals(key))
+                {
+                    itemFound = true;
+                    foundItem = item;
+                }
+            }
+            if (itemFound)
+            {
+                linkedList.Remove(foundItem);
+            }
+        }
     }
 }
