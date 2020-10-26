a125f93ed8fb2bc4cf91c3a5d76e27fd50408104
praveenupadhyay997 <praveenupadhyay997@gmail.com>
2020-10-26 06:43:00 +0530

    [Praveen] Add: Implemented the functionality to search a particular element in the binary tree.

--- HashTableAndBSTProblem/HashTableAndBSTProblem/BinaryTree.cs
+++ HashTableAndBSTProblem/HashTableAndBSTProblem/BinaryTree.cs
@@ -26,7 +26,7 @@ namespace HashTableAndBSTProblem
             this.leftTree = null;
             this.rightTree = null;
         }
-        //UC1- Inset the element in the specified node depending upon the value passed by user as item
+        //UC1- Insert the element in the specified node depending upon the value passed by user as item
         public void Insert(T item)
         {
             T currentNodeValue = this.nodeData;
@@ -35,23 +35,33 @@ namespace HashTableAndBSTProblem
             {
                 if (this.leftTree == null)
                 {
+                    //Creating a binary tree node
                     this.leftTree = new BinaryTree<T>(item);
+                    this.leftCount++;
                     Console.WriteLine("Inserting " + item);
                 }
                 else
+                {
+                    this.leftCount++;
+                    //Recursive call to progress in the insertion of the item
                     this.leftTree.Insert(item);
+                }       
             }
             //Inserting into the right tree in case the current node value is less than the passed value
             else if((currentNodeValue.CompareTo(item)) < 0)
             {
                 if (this.rightTree == null)
                 {
+                    //Creating a binary tree node
                     this.rightTree = new BinaryTree<T>(item);
+                    this.rightCount++;
                     Console.WriteLine("Inserting " + item);
                 }
                 else
                 {
+                    //Recursive call till the insertion is reached
                     this.rightTree.Insert(item);
+                    this.rightCount++;
                     Console.WriteLine("Inserting " + item);
                 }
             }
@@ -63,27 +73,48 @@ namespace HashTableAndBSTProblem
         /// </summary>
         public void Display()
         {
+            //Traversing the left binary tree
             if (this.leftTree != null)
             {
-                this.leftCount++;
                 this.leftTree.Display();
             }
-
+            //Displaying the items at the node
             Console.WriteLine(this.nodeData.ToString());
-
+            //Traversing the right binary tree
             if (this.rightTree != null)
             {
-                this.rightCount++;
                 this.rightTree.Display();
             }
         }
         /// <summary>
-        /// Function Returning the size of the binary tree
+        /// UC-5 Function Returning the size of the binary tree
         /// </summary>
         public void GetSize()
         {
             Console.WriteLine("Left Count Size = " + this.leftCount);
             Console.WriteLine("Right Count Size = "  + this.rightCount);
+            Console.WriteLine("Total elements in the binary tree =" + (this.leftCount+1+this.rightCount));
+        }
+        /// <summary>
+        /// UC6- Function to return whether the element is present in the binary tree or not
+        /// </summary>
+        /// <param name = "item" ></ param >
+        /// < returns ></ returns >
+        public void PresentOrNot(int item)
+        {
+            //Traversing the left binary tree
+            if (this.leftTree != null)
+            {
+                this.leftTree.PresentOrNot(item);
+            }
+            //Check condition for the element present in the binary tree
+            if(this.nodeData.ToString() == item.ToString())
+                Console.WriteLine("{0} is Present in the Binary Tree...",item);
+            //Traversing the right binary tree
+            if (this.rightTree != null)
+            {
+                this.rightTree.PresentOrNot(item);
+            }
         }
     }
 }
